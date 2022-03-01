6262a1b0095745aac9fc8f321b4ff19d30c08718
ZogsquaredPC <73807715+Zogsquared@users.noreply.github.com>
2022-03-02 11:51:23 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://www.nzdirectory.co.nz/robots.txt"
+BASE_URL = "https://www.nzdirectory.co.nz/automotives.html"
+
+response = requests.get(BASE_URL)
+#print(response.content)
+
+soup = BeautifulSoup(response.text,"html.parser")
+
+itemlist = soup.findAll("li", {"class":"premium"})
+
+for i in itemlist:
+    print(i)
