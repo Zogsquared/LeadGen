2f3d7ddab1adb529e9ead1c99e0eee630e47f4d9
ZogsquaredPC <73807715+Zogsquared@users.noreply.github.com>
2022-03-03 12:46:39 +1300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+global nlp
+info_list=[]
+nlp=[]
 
 BASE_URL = "https://www.nzdirectory.co.nz/automotives.html"
 
@@ -13,7 +16,22 @@ itemlist = soup.findAll("div", {"class":"listing_content"})
 print("Number of listings = " + str(len(itemlist)))
 
 for item in itemlist:
-    address = item.find("p", {"class":"address"})
-    print(address)
+    address = item.find("p", class_="address")
+    try:
+        if address is not None:
+            info_list.append([address.text])
+
+            details = address.text
+
+            i, a_phone = details.split('+', 1)
+            a_name, a_location = i.split(', ', 1)
+
+            nlp.append([a_name, a_location, a_phone])
+    except:
+        pass
+print(nlp)
+    #print(address)
+
+category = input("Enter Category name")
 
 print(len(itemlist))
